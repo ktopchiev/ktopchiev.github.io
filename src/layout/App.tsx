3adb07db8f512ba1b4d12db54cf9b953c58a09cf
Karol Topchiev <karlostopchiev@gmail.com>
2024-08-14 19:06:17 +0300

    styling and funcitonality to the header menu

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,17 +1,31 @@
-import { Container } from "@mui/material"
+import { Container, createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material"
 import Header from "./Header"
 import { Outlet } from "react-router-dom"
+import { useMemo } from "react";
 
 function App() {
-
+	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+	console.log(prefersDarkMode);
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: prefersDarkMode ? 'dark' : 'light',
+				},
+			}),
+		[prefersDarkMode],
+	);
 
 	return (
 
 		<>
-			<Header />
-			<Container>
-				<Outlet />
-			</Container>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Header />
+				<Container >
+					<Outlet />
+				</Container >
+			</ThemeProvider>
 		</>
 	)
 }
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,45 +1,131 @@
-import { AppBar, Box, Link, List, ListItem, Toolbar } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { NavLink } from 'react-router-dom';
+
+const navItems = [
+    { title: 'about', path: '/about' },
+    { title: 'projects', path: '/projects' },
+    { title: 'contacts', path: '/contacts' },
+
+];
 
 function Header() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
-    const navItems = [
-        { title: "about", path: '/about' },
-        { title: "projects", path: '/projects' },
-        { title: "contacts", path: '/contacts' }
-    ]
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-    const navStyles = {
-        color: "inherit",
-        '&:hover': { color: 'black' },
-        textDecoration: 'none'
-    }
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
         <AppBar position="fixed">
-            <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <Container >
+                <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component={NavLink}
+                        to={'/'}
+                        sx={{
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            '&:hover': { color: 'text.disabled' }
+                        }}
+                    >
+                        K.Topchiev
+                    </Typography>
 
-                <Link
-                    component={NavLink}
-                    to={"/"}
-                    key={"/"}
-                    sx={navStyles}
-                >
-                    K.Topchiev
-                </Link>
-
-                <Box>
-                    <List sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        {navItems.map(({ title, path }) =>
-                            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                    <Box sx={{ flexGrow: 5, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {navItems.map(({ title, path }) => (
+                                <MenuItem key={path} onClick={handleCloseNavMenu}>
+                                    <Typography component={NavLink} to={path} textAlign="center">{title.toUpperCase()}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component={NavLink}
+                        to={'/'}
+                        sx={{
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'Monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        K.Topchiev
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {navItems.map(({ title, path }) => (
+                            <Button
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    color: 'white',
+                                    display: 'block',
+                                    '&:hover': { color: 'text.disabled' },
+                                    '&.active': { color: '#88edff ' }
+                                }}
+                            >
                                 {title.toUpperCase()}
-                            </ListItem>
-                        )}
-                    </List>
-                </Box>
-            </Toolbar>
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
         </AppBar>
-    )
+    );
 }
-
-export default Header
+export default Header;
