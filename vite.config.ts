63468f26000e38b6f75c2118368a7d4060c804d5
Karol Topchiev <karlostopchiev@gmail.com>
2024-09-13 02:51:18 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/[ktopchiev.github.io]/"
+  base: '/ktopchiev.github.io/',
+  build: {
+    target: 'esnext',
+  },
+  plugins: [react()]
 })
