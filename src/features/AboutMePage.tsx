5a710b8fe4b90389baad2de97b1debf0b4e45cef
Karol Topchiev <karlostopchiev@gmail.com>
2024-09-18 14:56:55 +0300

    .

--- src/features/AboutMePage.tsx
+++ src/features/AboutMePage.tsx
@@ -36,26 +36,26 @@ function AboutMePage() {
 					but recently I’ve been diving into Domain-Driven Design and Clean Code concepts through self-study to level up my skills.
 				</Typography>
 
-				<CenterFocusStrongOutlinedIcon fontSize="large" sx={{ color: '#6eccfa' }}></CenterFocusStrongOutlinedIcon>
+				<CenterFocusStrongOutlinedIcon fontSize="large" sx={{ marginTop: '28px', color: '#6eccfa' }}></CenterFocusStrongOutlinedIcon>
 				<Typography >
 					I’m passionate about building efficient, maintainable code and constantly learning. Right now,
 					I’m focusing on improving my frontend expertise with React.
 				</Typography>
 
-				<SelfImprovementOutlinedIcon fontSize="large" sx={{ color: '#6eccfa' }}></SelfImprovementOutlinedIcon>
+				<SelfImprovementOutlinedIcon fontSize="large" sx={{ marginTop: '28px', color: '#6eccfa' }}></SelfImprovementOutlinedIcon>
 				<Typography >
 					In addition to my technical knowledge, I’m a quick learner and self-motivated.
 					I enjoy working with teams and love tackling problems through critical thinking.
 					I’m constantly looking for ways to grow, both personally and professionally.
 				</Typography>
 
-				<SportsEsportsOutlinedIcon fontSize="large" sx={{ color: '#6eccfa' }}></SportsEsportsOutlinedIcon>
+				<SportsEsportsOutlinedIcon fontSize="large" sx={{ marginTop: '28px', color: '#6eccfa' }}></SportsEsportsOutlinedIcon>
 				<Typography >
 					Aside from coding, I have a few creative outlets. I enjoy listening to music, playing guitar, drawing, and watching movies.
 					I also love reading books whenever I get the chance. These hobbies keep me inspired and give me balance outside of tech.
 				</Typography >
 
-				<Typography >
+				<Typography sx={{ marginTop: '28px' }}>
 					Feel free to check out my projects and <Link component={NavLink} to={'/connect'}>connect</Link> with me!
 				</Typography >
 
